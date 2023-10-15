27c17b218bcba14402e2f503e622fd02a4f670a1
gavrilchuk <gavrilchuk_i@itstep.academy>
2023-10-15 14:53:01 +0300

    firsrdfghdfg

--- 1/main.py
+++ 1/main.py
@@ -1,2 +1,5 @@
-print("Hello")
-#fdjghklj
+class trikutnik:
+    type="трикутник"
+    count=3
+tr1=trikutnik()
+print(tr1.type)
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Hello")
-#fdjghklj
+class trikutnik:
+    type="трикутник"
+    count=3
+    name="ABC"
+    def __init__(self,name,a,b,c):
+        self.name=name
+        self.a=a
+        self.b=b
+        self.c=c
+
+
+
+    def print_tr(self):
+        print(f"Моє ім'я - {self.name}")
+    def remane(self,new_name):
+        self.name=new_name
+        self.print_tr()
+
+tr=trikutnik('ABC',2,3,4)
+
+tr1.print_tr()
